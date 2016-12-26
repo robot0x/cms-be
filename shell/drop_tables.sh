a2fea40ac4193b6dbc943899616b00e10cf9cd27
lyf <1601942611@qq.com>
2016-12-26 13:08:37 +0800

    更新shell脚本

---
+++ shell/drop_tables.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+drop table article_meta;
+drop table article_content;
+drop table author;
+drop table image;
--- shell/mysql/article_meta.sql
+++ shell/mysql/article_meta.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `article_meta` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-  `nid` int(10) unsigned NOT NULL COMMENT '文章的短id',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `nid` int(11) unsigned NOT NULL unique COMMENT '文章的短id',
   `title` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文章title',
-  `ctype` tinyint(2) unsigned COMMENT '文章类型',
-  `create_time` timestamp(0) NOT NULL COMMENT '商品创建时间',
-  `last_update_time` timestamp(0) NOT NULL COMMENT '商品最后更新时间',
-  `author` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文章作者id',
-  `lock_by` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '被那个作者锁定',
-  `last_update_by` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '被那个作者锁定',
+  `ctype` tinyint(2) unsigned NOT NULL COMMENT '文章类型',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '文章创建时间',
+  `last_update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '文章最后更新时间',
+  `author` int(11) unsigned COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '文章作者id',
+  `lock_by` int(11) unsigned COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '被那个作者锁定',
+  `last_update_by` int(11) unsigned COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '被那个作者锁定',
   `data` text COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '冗余字段',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='文章meta表';
