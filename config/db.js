8f857e8744378899da05d5b27e51a89b1a8ecba5
lyf <1601942611@qq.com>
2017-05-28 15:37:21 +0800

    up

--- config/db.js
+++ config/db.js
@@ -7,25 +7,27 @@
 //     "multipleStatement":true,
 //     "connectionLimit": 15
 //  }
- const isDebug =  process.env.NODE_ENV === 'dev' ? true : false
+const isDebug = process.env.NODE_ENV === 'dev'
 
- module.exports = isDebug ?  {
-     "host": 'localhost',
-     "user":"root",
-     "password":"sqltest",
-     "database":"cms",
-     "charset":"utf8mb4",
-     "multipleStatement":true,
-     "connectionLimit": 15
-  } : {
-    'host': 'rds7bz3av7bz3av.mysql.rds.aliyuncs.com',
-    //  reader是只读帐号，渲染相关的都是只需要读就可以了
-    // 'user': 'reader',
-    // 'password': 'reader',
-    'user': 'diaodiao',
-    'password': 'diaodiao',
-    'database': 'diaodiao',
-    'charset': 'utf8mb4',
-    'multipleStatement': true,
-    'connectionLimit': 15
+module.exports = isDebug
+  ? {
+    host: 'localhost',
+    user: 'root',
+    password: 'sqltest',
+    database: 'cms',
+    charset: 'utf8mb4',
+    multipleStatement: true,
+    connectionLimit: 15
+  }
+  : {
+    host: 'rds7bz3av7bz3av.mysql.rds.aliyuncs.com',
+      //  reader是只读帐号，渲染相关的都是只需要读就可以了
+      // 'user': 'reader',
+      // 'password': 'reader',
+    user: 'diaodiao',
+    password: 'diaodiao',
+    database: 'diaodiao',
+    charset: 'utf8mb4',
+    multipleStatement: true,
+    connectionLimit: 15
   }
